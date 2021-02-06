526c5df6d0ded35961e010eaebab5917842fea89
Brian Flieck <flieckb@KOP01-BflieckMacBookPro2016.local>
2021-02-06 12:47:49 -0500

    updated youtube api

--- homepagebasics/md-script.js
+++ homepagebasics/md-script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+
+   
+
     //get movie info from local storage/api
     let data = JSON.parse(localStorage.getItem("movieClicked"));
     console.log(data);
@@ -7,39 +10,14 @@ $(document).ready(function () {
     summary = data.overview;
     movieName = data.title;
     movieYear = (data.release_date).substring(0, 4);
-    movieRating = data.vote_average;
     posterUrl = "https://image.tmdb.org/t/p/w154" + posterPath;
+    videoSearch(movieName)
 
     //display elements dynamically
     $("#poster").attr("src", posterUrl);
     $("#synopsis").text(summary);
     $("#movie-title").text(movieName);
-    $("#year").text("Release date: " + movieYear);
-    $("#rating").text("Rating: " + movieRating);
-
-    //checking to see if movie is already in watch list, then disable button so it cant be added twice
-    if (localStorage.getItem("watchList") != null) {
-        let watchListCheck = JSON.parse(localStorage.getItem("watchList"));
-        for (i = 0; i < watchListCheck.length; i++) {
-            if (watchListCheck[i].title === data.title) {
-                $("#addToWatch").text("Added to watchlist");
-                $("#addToWatch").removeClass("w3-black").addClass("w3-green");
-                $("#addToWatch").prop("disabled", true);
-            }
-        }
-    }
-
-    //checking to see if movie is already in movies seen list, then disable button so it cant be added twice
-    if (localStorage.getItem("moviesSeen") != null) {
-        let moviesSeenCheck = JSON.parse(localStorage.getItem("moviesSeen"));
-        for (i = 0; i < moviesSeenCheck.length; i++) {
-            if (moviesSeenCheck[i].title === data.title) {
-                $("#markAsSeen").text("Marked as seen");
-                $("#markAsSeen").removeClass("w3-black").addClass("w3-green");
-                $("#markAsSeen").prop("disabled", true);
-            }
-        }
-    }
+    $("#year").text(movieYear);
 
     //click event for add to wactchlist button
     $("#addToWatch").click(function () {
@@ -76,6 +54,30 @@ $(document).ready(function () {
         $("#markAsSeen").removeClass("w3-black").addClass("w3-green");
         $("#markAsSeen").prop("disabled", true);
     });
+
+    function videoSearch(MovieName) {
+
+    //youtube api variables
+    var YOUTUBE_API_KEY = "AIzaSyDWRD2C1LDceUrtIs4HrOwEiOCPBegSyUs";
+    var video = '';
+    var MovieTitle = MovieName
+    var maxResults = "1";
+    var player1 = '<iframe width="560" height="315" src="https://www.youtube.com/embed/';
+    var player2 = '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+  
+        fetch("https://www.googleapis.com/youtube/v3/search?&key=" + YOUTUBE_API_KEY + "&type=video&maxResults=" + maxResults + "&q=" + MovieTitle + " trailer")
+
+            .then(Response => Response.json())
+            .then(data => {
+                var videoId = data['items'][0]['id']['videoId'];
+                console.log("video id: " + videoId);
+                console.log(data);
+                video = player1 + videoId + player2;
+                $("#trailer-video").append(video)
+            })
+
+    }
 
 
 })
