8e0cdc1378c9167ffd70b5555fecd5978c57297b
burtk33 <burtk33@gmail.com>
2021-02-06 14:27:29 -0500

    added functionality for watchlist rendering and buttons to mark as seen

--- assets/js/md-script.js
+++ assets/js/md-script.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-
-   
-
     //get movie info from local storage/api
     let data = JSON.parse(localStorage.getItem("movieClicked"));
     console.log(data);
@@ -10,14 +7,40 @@ $(document).ready(function () {
     summary = data.overview;
     movieName = data.title;
     movieYear = (data.release_date).substring(0, 4);
+    movieRating= data.vote_average;
     posterUrl = "https://image.tmdb.org/t/p/w154" + posterPath;
-    videoSearch(movieName)
+    videoSearch(movieName);
 
     //display elements dynamically
     $("#poster").attr("src", posterUrl);
     $("#synopsis").text(summary);
     $("#movie-title").text(movieName);
     $("#year").text(movieYear);
+    $("#rating").text("Rating: "+movieRating+"/10");
+
+    //checking to see if movie is already in watch list, then disable button so it cant be added twice
+    if (localStorage.getItem("watchList") != null) {
+        let watchListCheck = JSON.parse(localStorage.getItem("watchList"));
+        for (i = 0; i < watchListCheck.length; i++) {
+            if (watchListCheck[i].title === data.title) {
+                $("#addToWatch").text("Added to watchlist");
+                $("#addToWatch").removeClass("w3-black").addClass("w3-green");
+                $("#addToWatch").prop("disabled", true);
+            }
+        }
+    }
+
+    //checking to see if movie is already in movies seen list, then disable button so it cant be added twice
+    if (localStorage.getItem("moviesSeen") != null) {
+        let moviesSeenCheck = JSON.parse(localStorage.getItem("moviesSeen"));
+        for (i = 0; i < moviesSeenCheck.length; i++) {
+            if (moviesSeenCheck[i].title === data.title) {
+                $("#markAsSeen").text("Marked as seen");
+                $("#markAsSeen").removeClass("w3-black").addClass("w3-green");
+                $("#markAsSeen").prop("disabled", true);
+            }
+        }
+    }
 
     //click event for add to wactchlist button
     $("#addToWatch").click(function () {
@@ -55,12 +78,12 @@ $(document).ready(function () {
         $("#markAsSeen").prop("disabled", true);
     });
 
-    function videoSearch(MovieName) {
+    function videoSearch(movieName) {
 
     //youtube api variables
     var YOUTUBE_API_KEY = "AIzaSyDWRD2C1LDceUrtIs4HrOwEiOCPBegSyUs";
     var video = '';
-    var MovieTitle = MovieName
+    var MovieTitle = movieName
     var maxResults = "1";
     var player1 = '<iframe width="560" height="315" src="https://www.youtube.com/embed/';
     var player2 = '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
@@ -74,7 +97,7 @@ $(document).ready(function () {
                 console.log("video id: " + videoId);
                 console.log(data);
                 video = player1 + videoId + player2;
-                $("#trailer-video").append(video)
+                $("#trailer-video").append(video);
             })
 
     }
--- assets/js/wl-script.js
+++ assets/js/wl-script.js
@@ -1,12 +1,93 @@
 let watchList = JSON.parse(localStorage.getItem("watchList"));
 let moviesSeen = JSON.parse(localStorage.getItem("moviesSeen"));
 
+//populate watch list dynamically
+if (watchList != null) {
+    for (i = 0; i < watchList.length; i++) {
+        let posterPath = watchList[i].poster_path;
+        let summary = watchList[i].overview;
+        let movieName = watchList[i].title;
+        let movieYear = (watchList[i].release_date).substring(0, 4);
+        let movieRating = watchList[i].vote_average;
+        let posterUrl = "https://image.tmdb.org/t/p/w154" + posterPath;
 
-for (i = 0; i < watchList.length; i++) {
-    posterPath = wactchList[i].poster_path;
-    summary = watchList[i].overview;
-    movieName = watchList[i].title;
-    movieYear = (watchList[i].release_date).substring(0, 4);
-    movieRating = watchList[i].vote_average;
-    posterUrl = "https://image.tmdb.org/t/p/w154" + posterPath;
+        var movieNameDiv = $("<h2>");
+        var movieDateDiv = $("<h3>");
+        var movieSynopsisDiv = $("<p>");
+        var moviePosterDiv = $("<img>");
+        var movieRatingDiv = $("<p>");
+
+        movieNameDiv.text(movieName)
+        movieDateDiv.text(movieYear)
+        movieSynopsisDiv.text(summary);
+        moviePosterDiv.attr("src", posterUrl)
+
+        movieRatingDiv.text("Rating: " + movieRating)
+
+        //dynamically create the card/panel to hold results
+        var movieInfo = $("<div>").addClass("w3-panel w3-black w3-border")
+        movieInfo.val(i);
+        var posterColumn = $("<div>").addClass("w3-col l4 w3-center")
+        var infoColumn = $("<div>").addClass("w3-col l8")
+        var posterCard = $("<div>").addClass("w3-card w3-white w3-padding w3-margin w3-center")
+        var infoCard = $("<div>").addClass("w3-card w3-white w3-padding-small w3-margin")
+        var removeBtn = $("<button>").addClass("w3-button w3-red w3-round-xlarge remove").text("Remove from watchlist");
+        var markSeenBtn = $("<button>").addClass("w3-button w3-green w3-round-xlarge seen").text("Mark as seen");
+        markSeenBtn.val(i);
+
+        posterCard.append(moviePosterDiv)
+        infoCard.append(movieNameDiv, movieDateDiv, movieRatingDiv, movieSynopsisDiv)
+
+        posterColumn.append(posterCard)
+        infoColumn.append(infoCard)
+        movieInfo.append(posterColumn, infoColumn)
+
+        $("#watchListResults").append(movieInfo)
+        $("#watchListResults").append(removeBtn, markSeenBtn);
+
+    }
 }
+
+if (moviesSeen != null) {
+    for (i = 0; i < moviesSeen.length; i++) {
+
+        let movieName = moviesSeen[i].title;
+        let movieYear = (moviesSeen[i].release_date).substring(0, 4);
+        let movieNameDiv = $("<h4>");
+        console.log(moviesSeen);
+        movieNameDiv.text(movieName + " (" + movieYear + ")")
+
+        //dynamically create the card/panel to hold results
+        $("#moviesSeenResults").append(movieNameDiv)
+    }
+}
+
+
+$(".w3-panel").click(function () {
+    let selected = this.value;
+    console.log(selected);
+    localStorage.setItem("movieClicked", JSON.stringify(watchList[selected]))
+    window.location.href = "movie-detail.html";
+});
+
+$(".seen").click(function () {
+    let btnValue = this.value;
+    console.log(btnValue);
+    if (localStorage.getItem("moviesSeen") != null) {
+        let existing = JSON.parse(localStorage.getItem("moviesSeen"));
+        existing.push(watchList[btnValue]);
+        console.log(existing);
+        localStorage.setItem("moviesSeen", JSON.stringify(existing));
+    }
+    else {
+        let seenArray = [];
+        seenArray.push(watchList[btnValue]);
+        localStorage.setItem("moviesSeen", JSON.stringify(seenArray));
+    }
+    watchList.splice(btnValue, 1);
+    localStorage.setItem("watchList", JSON.stringify(watchList));
+
+    $("#markSeen").text("Marked as seen");
+    $("#markSeen").removeClass("w3-green").addClass("w3-black");
+    $("#markSeen").prop("disabled", true);
+});
